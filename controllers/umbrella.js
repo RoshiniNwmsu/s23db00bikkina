949c239f992aba60bfb6d0d379854e4707ab11b4
RoshiniNwmsu <roshinibikkina14@gmail.com>
2023-04-19 19:58:56 -0500

    hii

--- controllers/umbrella.js
+++ controllers/umbrella.js
@@ -110,3 +110,29 @@ exports.umbrella_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+   // Handle a show one view with id specified by query
+exports.umbrella_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await umbrella.findById( req.query.id)
+    res.render('umbrelladetail',
+   { title: 'umbrella Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+}
+};
+// Handle building the view for creating a umbrella.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.umbrella_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('umbrellacreate', { title: 'umbrella Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/umbrella.js
+++ routes/umbrella.js
@@ -3,5 +3,9 @@ const umbrella_controlers= require('../controllers/umbrella');
 var router = express.Router();
 /* GET umbrella */
 // GET request for one umbrella.
-router.get('/umbrella/:id', umbrella_controlers.umbrella_detail);
+router.get('/', umbrella_controlers.umbrella_detail);
 module.exports = router;
+/* GET detail umbrella page */
+router.get('/detail', umbrella_controlers.umbrella_view_one_Page);
+/* GET create umbrella page */
+router.get('/create', umbrella_controlers.umbrella_create_Page);
